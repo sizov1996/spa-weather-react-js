406988fad466a2efa9bd6f2d3f057a281b42e5bd
sizov-macbook <test@MacBook-Pro-test.local>
2022-06-06 16:47:59 +0400

    add useRef

--- src/Input/index.js
+++ src/Input/index.js
@@ -1,17 +1,20 @@
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
 import '../App.css';
 
 export const Input = ({ setCitiesList }) => {
-    const [inputValue, setInputValue] = useState('empty');
+    const [inputValue, setInputValue] = useState('');
+    const inputRef = useRef(null);
     const handleOnClick = () => {
         setCitiesList((currentArray)=>[...currentArray, inputValue])
+        setInputValue('');
+        inputRef.current.focus();
     }
     const handleOnChange = (event) => {
         setInputValue(event.target.value);
     }
     return (
         <div className='InputWrap'>
-            <input className='Input' onChange={handleOnChange} value={inputValue} />
+            <input className='Input' onChange={handleOnChange} value={inputValue}  ref={inputRef}/>
             <button className='Button' onClick={handleOnClick}>+</button>
         </div>
     );
